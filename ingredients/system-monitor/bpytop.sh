f42d1bf6c22cdcd8c6236b782912785e0bbf3764
Robert <rstrube@gmail.com>
2021-10-27 08:14:02 -0600

    Switched from bpytop to btop

--- ingredients/system-monitor/bpytop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#|#./ingredients/system-monitor/bpytop.sh #bpytop: a terminal based system monitoring tool (like htop but better)
-
-paru -S --noconfirm --needed bpytop
---
+++ ingredients/system-monitor/btop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#|#./ingredients/system-monitor/btop.sh #btop: a terminal based system monitoring tool (like htop but better)
+
+paru -S --noconfirm --needed btop
