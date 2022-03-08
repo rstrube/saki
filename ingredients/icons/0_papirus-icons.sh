aa3e1faa2d8e79f7ef1808facea542fcc21d60a6
Robert <rstrube@gmail.com>
2022-03-08 15:33:20 -0700

    Minor tweak to papirus-icons ingredient.  Better to have icon theme set manually via KDE settings.

--- ingredients/icons/0_papirus-icons.sh
+++ ingredients/icons/0_papirus-icons.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #|#./ingredients/icons/0_papirus-icons.sh #Papirus icon theme
 
+DIR=$(dirname "$0")
+source $DIR/../_helper/_common-functions.sh
+
 paru -S --noconfirm --needed papirus-icon-theme
 
 # Configure KDE to use the Papirus icon theme
-/usr/lib/plasma-changeicons "Papirus-Dark"
+# /usr/lib/plasma-changeicons "Papirus-Dark"
+
+echo -e "${YELLOW}Warning: you will need to configure Papirus as your icon theme via settings.${NC}"
