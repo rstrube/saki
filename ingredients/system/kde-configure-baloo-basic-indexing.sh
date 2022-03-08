2b0e705eaff80f287977e02e5c0ea532a5e01395
Robert <rstrube@gmail.com>
2022-03-08 15:44:39 -0700

    Minor tweak to baloo configuration ingredient

--- ingredients/system/kde-configure-baloo-basic-indexing.sh
+++ ingredients/system/kde-configure-baloo-basic-indexing.sh
@@ -2,3 +2,12 @@
 #|#./ingredients/system/kde-configure-baloo-basic-indexing.sh #Configure Baloo file indexer to only perform basic indexing (performance improvement)
 
 kwriteconfig5 --file baloofilerc --group General --key "only basic indexing" "true"
+
+# Purge the existing index
+balooctl purge
+
+# Reindex files
+balooctl check
+
+# Report status
+balooctl status
