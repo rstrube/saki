0af87f73d96d3cbb3ecb31fe33a9a62bcb356d82
Robert <rstrube@gmail.com>
2022-03-08 18:36:54 -0700

    Updated obsidian ingredient to set electron-flags.conf

--- ingredients/productivity/obsidian.sh
+++ ingredients/productivity/obsidian.sh
@@ -2,3 +2,11 @@
 #|#./ingredients/productivity/obsidian.sh #Excellent markdown based note manager and "second brain"
 
 paru -S --noconfirm --needed obsidian
+
+cat <<EOT > "electron-flags.conf"	
+--enable-features=UseOzonePlatform
+--ozone-platform=wayland
+EOT
+
+cp electron-flags.conf ~/.config/
+rm electron-flags.conf
