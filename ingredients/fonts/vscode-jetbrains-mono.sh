8c20761b2235449897710433b951eea08c513410
Robert <rstrube@gmail.com>
2022-04-12 16:18:35 -0600

    Updated vscode ingredients

--- ingredients/fonts/vscode-jetbrains-mono.sh
+++ ingredients/fonts/vscode-jetbrains-mono.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #|#./ingredients/fonts/vscode-jetbrains-mono.sh #JetBrains Mono font for Visual Studio Code [Requires /dev/1_vscode and /fonts/0_xxx-fonts ingredients]
 
+DIR=$(dirname "$0")
+source $DIR/../dev/_helper/_vscode-functions.sh
+
 sed -i '$i\    "editor.fontFamily": "JetBrains Mono",' "$VSCODE_SETTINGS_FILE_PATH"
 sed -i '$i\    "editor.fontWeight": "500",' "$VSCODE_SETTINGS_FILE_PATH"
 sed -i '$i\    "editor.fontLigatures": true,' "$VSCODE_SETTINGS_FILE_PATH"
--- recipe.sh
+++ recipe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# recipe.sh : 2022-03-25-20:48:48
+# recipe.sh : 2022-04-12-16:16:55
 # NOTE: Please uncomment the ingredients you wish to install before running!
 # --------------------------------------------------------------------------
 
