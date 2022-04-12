9f9e4fc72da4f410526fddb8ea9b9fefa5403949
Robert <rstrube@gmail.com>
2022-04-12 16:36:26 -0600

    More refinements to both vscode and obsidian ingredients

--- ingredients/dev/_helper/_vscode-functions.sh
+++ ingredients/dev/_helper/_vscode-functions.sh
@@ -18,7 +18,22 @@ function create_empty_vscode_settings_if_neccessary {
     fi
 }
 
+function update_code_desktop_file_for_wayland {
+
+    # Copy the system .desktop file to your $HOME and tweak it to launch VSCode as a native Wayland application
+    if [[ ! -d "~/.local/share/applications" ]]; then
+        mkdir -p ~/.local/share/applications
+    fi
+
+    cp /usr/share/applications/visual-studio-code.desktop ~/.local/share/applications/.
+    sed -i 's/\/usr\/bin\/code/& --enable-features=UseOzonePlatform --ozone-platform=wayland/' ~/.local/share/applications/visual-studio-code.desktop
+}
+
 function create_and_configure_code_flags_for_wayland {
+
+    if [[ ! -d "~./config" ]]; then
+        mkdir -p ~/.config
+    fi
 
     cat <<EOT > "code-flags.conf"	
 --enable-features=UseOzonePlatform
--- ingredients/productivity/obsidian-wayland.sh
+++ ingredients/productivity/obsidian-wayland.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 #|#./ingredients/productivity/obsidian-wayland.sh #Configure Obsidian to run as a native Wayland application
 
-if [[ ! -d "~./config" ]]; then
-    mkdir -p ~/.config
-fi
+function create_and_configure_electron_flags_for_wayland {
 
-cat <<EOT > "electron-flags.conf"	
+    if [[ ! -d "~./config" ]]; then
+        mkdir -p ~/.config
+    fi
+
+    cat <<EOT > "electron-flags.conf"	
 --enable-features=UseOzonePlatform
 --ozone-platform=wayland
 EOT
 
-cp electron-flags.conf ~/.config/
-rm electron-flags.conf
+    cp electron-flags.conf ~/.config/
+    rm electron-flags.conf
+}
+
+function update_obsidian_desktop_file_for_wayland {
+
+    # Copy the system .desktop file to your $HOME and tweak it to launch VSCode as a native Wayland application
+    if [[ ! -d "~/.local/share/applications" ]]; then
+        mkdir -p ~/.local/share/applications
+    fi
+
+    cp /usr/share/applications/obsidian.desktop ~/.local/share/applications/.
+    sed -i 's/\/usr\/bin\/obsidian/& --enable-features=UseOzonePlatform --ozone-platform=wayland/' ~/.local/share/applications/obsidian.desktop
+}
+
+create_and_configure_electron_flags_for_wayland
+update_obsidian_desktop_file_for_wayland
