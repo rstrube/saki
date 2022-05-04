9995ba891f0dea3fb30c61c0e218bec523480015
Robert <rstrube@gmail.com>
2022-05-04 14:18:53 -0600

    Minor tweak to the firefox ingredient to callout using native filepicker

--- ingredients/web/firefox.sh
+++ ingredients/web/firefox.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #|#./ingredients/web/firefox.sh #Firefox
 
+DIR=$(dirname "$0")
+source $DIR/../_helper/_common-functions.sh
+
 paru -S --noconfirm --needed firefox
+
+echo -e "${YELLOW}Additional about:config tweaks worth considering:${NC}"
+echo -e "${YELLOW}widget.use-xdg-desktop-portal.file-picker=1 : Use native file picker via XDG desktop portal${NC}"
